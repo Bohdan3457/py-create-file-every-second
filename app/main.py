fbb832a4f1f790f7672690df254ddaa7ebb16613
Andrii Bovsunovskyi <an.bvsn@gmail.com>
2026-02-15 16:53:06 +0200

    Update README.md

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        current_time = datetime.now()
+        hours = current_time.strftime("%H")
+        minutes = current_time.strftime("%M")
+        seconds = current_time.strftime("%S")
+
+        file_name = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(file_name, "w") as name_file:
+            name_file.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+
+        print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}, {file_name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
